ad7c692b8f7d20b989dc7596ca45a86de87ea9a5
ybliu <liuyubiaowu@qq.com>
2017-08-28 18:36:27 +0800

    init

---
+++ algorithm/计算数字出现次数.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	char b;
+	cin >> a >> b;
+	string str = "";
+	string c;
+	for (int i = 1; i <= a; i++)
+	{
+		c = std::to_string(i);
+		str += c;
+	}
+	int k = 0;
+
+	for (int i = 0; i < str.length(); i++)
+	{	
+		char d = str[i];
+		if (b == d)
+		{
+			k++;
+		}
+	}
+	cout << k << endl;
+}
