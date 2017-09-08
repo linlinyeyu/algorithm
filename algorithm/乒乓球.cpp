cc155f76b731dab36a0d5d4303c2e7ed5c0edc85
ybliu <liuyubiaowu@qq.com>
2017-09-08 18:00:19 +0800

    dau

--- algorithm/乒乓球.cpp
+++ algorithm/乒乓球.cpp
@@ -4,6 +4,48 @@
 
 using namespace std;
 
-void countWin() {
+void count11(char str[]) {
+	int score1 = 0,score2=0;
+	for (int i = 0; i < sizeof(str); i++) {
+		if (str[i] == 'W')
+		{
+			score1++;
+		}
+		if (str[i] == 'L') {
+			score2++;
+		}
 
+		if (score1 == 11 || score2 == 11) 
+		{
+			cout << score1 << ':' << score2<<endl;
+			score1 = 0;
+			score2 = 0;
+		}
+	}
+}
+
+void count21(char *str) {
+
+}
+
+void countWin() {
+	char *str;
+	int k = 0;
+	while (true) {
+		char a = getchar();
+		if (a != EOF)
+		{
+			if (a == 'E')
+			{
+				break;
+			}
+			else {
+				str += a;
+			}
+		}
+	}
+
+	count11(str);
+	cout << endl << endl;
+	count21(str);
 }
