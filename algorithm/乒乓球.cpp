a1723401fd7beefefca7e5d2e01de4a9169a51a7
linlinyeyu <liuyubiaowu@qq.com>
2017-09-10 23:52:19 +0800

    dau

--- algorithm/乒乓球.cpp
+++ algorithm/乒乓球.cpp
@@ -24,23 +24,35 @@ void count11(char str[]) {
 	}
 }
 
-void count21(char *str) {
-
+void count21(char str[]) {
+	int score1 = 0,score2=0;
+	for (int i = 0; i < sizeof(str); i++)
+	{
+		if (str[i] == 'W') {
+			score1++;
+		}
+		if (str[i] == 'L') {
+			score2++;
+		}
+		if (score1 == 21 || score2 == 21)
+		{
+			cout << score1 << ':' << score2 << endl;
+			score1 = 0;
+			score2 = 0;
+		}
+	}
 }
 
 void countWin() {
-	char *str;
+	char str[10000];
+	int i = 0;
 	int k = 0;
 	while (true) {
-		char a = getchar();
-		if (a != EOF)
+		if ((str[i] = getchar()) != EOF)
 		{
-			if (a == 'E')
+			if (str[i] == 'E')
 			{
 				break;
-			}
-			else {
-				str += a;
 			}
 		}
 	}
